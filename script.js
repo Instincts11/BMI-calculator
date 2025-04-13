f3ed4430dd67e4c4d46e74e16a5d92d3a3138aba
instincts11 <amalvr11@gmail.com>
2025-04-13 09:24:01 +0530

    Resolved merge conflicts and updated files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 const form=document.querySelector('form')
 
@@ -19,26 +19,5 @@ form.addEventListener('submit',function(e){
         //let bmi = (weight / ((height / 100) ** 2)).toFixed(4);
          result.innerHTML=`<span>${bmi}</span>`
     }
-=======
 
-const form=document.querySelector('form')
-
-form.addEventListener('submit',function(e){
-    e.preventDefault()
-    
-    const height=parseInt(document.querySelector('#height').value)
-    const weight=parseInt(document.querySelector('#weight').value)
-    const result=document.querySelector('#result')
-
-    if (height===''||height<0||isNaN(height)) {
-        result.innerHTML=`The height is ${height}`
-    }
-    else if (weight===''||weight<0||isNaN(weight)) {
-        result.innerHTML=`The weight is ${weight}`
-    }
-    else {
-        //let bmi = (weight / ((height / 100) ** 2)).toFixed(4);
-         result.innerHTML=`<span>${bmi}</span>`
-    }
->>>>>>> 0637344 (first commit)
 })
